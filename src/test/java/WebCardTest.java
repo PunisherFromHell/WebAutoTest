53fc9f9166bc802ba3a7c89f7b4cb09e16660f51
mega test <mahp0@mail.ru>
2024-04-14 13:17:28 +0300

    some things fixed

--- src/test/java/WebCardTest.java
+++ src/test/java/WebCardTest.java
@@ -44,7 +44,7 @@ class WebCardTest {
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("[type=button]")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text);
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
 
         // Your test logic here
     }
